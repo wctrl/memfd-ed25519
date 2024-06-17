2911fab239d5c2024d20b0a438f4c552bbb36e25
wctrl <166127285+wctrl@users.noreply.github.com>
2024-06-17 21:04:18 +0300

    Minor changes

--- src/make_signature.rs
+++ src/make_signature.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use std::fs;
-use ed25519_dalek::{ed25519::signature::SignerMut, SigningKey};
+use ed25519_dalek::{Signer, SigningKey};
 
 const PRIVATE_KEY: [u8; 32] = [79, 138, 237, 196, 27, 41, 3, 1, 216, 72, 140, 3, 112, 31, 47, 159, 95, 3, 252, 186, 205, 20, 6, 142, 146, 116, 34, 99, 128, 128, 125, 210];
 
@@ -8,7 +8,7 @@ fn main() {
     let main_rs = fs::read_to_string("src/main.rs").expect("Can't read main.rs");
     let app_o = fs::read("src/app.o").expect("Can't read app.o");
 
-    let mut signing_key = SigningKey::from_bytes(&PRIVATE_KEY);
+    let signing_key = SigningKey::from_bytes(&PRIVATE_KEY);
     let signature = signing_key.sign(&app_o);
     
     let replaced = main_rs.replace("[0; 64];", format!("{:?};", signature.to_vec()).as_str());
